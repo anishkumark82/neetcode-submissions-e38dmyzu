27dfe234537892ff6dbe4ad73f9cb3d21badfbf1
anishkumark82 <anishkumark@gmail.com>
2026-08-14 18:13:42 -0400

    Add: clone-graph - submission-12

---
+++ Data Structures & Algorithms/clone-graph/submission-12.cpp
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if(node == nullptr)
+            return nullptr;
+        
+        // Old to new Node mapping
+        unordered_map<Node*, Node*> cloneMap;
+        // Node* node is an adjList
+        queue<Node *> Q;
+        Q.push(node);
+
+        while(!Q.empty())
+        {
+            int qSize = Q.size();
+            for(auto i = 0; i < qSize; ++i)
+            {
+                Node *n = Q.front();
+                Q.pop();
+                if(!cloneMap.count(n))
+                    cloneMap[n] = new Node(n->val); 
+                
+                for(auto &nbr : n->neighbors)
+                {
+                    if(!cloneMap.count(nbr))
+                    {
+                        cloneMap[nbr] = new Node(nbr->val);
+                        Q.push(nbr);
+                    }
+                    cloneMap[n]->neighbors.push_back(cloneMap[nbr]);
+                }
+            }
+        }
+        return cloneMap[node];
+    }
+};
